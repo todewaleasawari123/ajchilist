c72d409fd7d44e298cd1a9288baf73919a093fd9
NISHAD <NISHAD@DESKTOP-7B1O3SH>
2022-06-04 21:31:44 +0530

    kathin kathin kathin kiti

---
+++ src/main/java/pom/flipcart/com/NewPOm.java
@@ -0,0 +1,13 @@
+package pom.flipcart.com;
+
+import org.testng.annotations.Parameters;
+
+public class NewPOm {
+	
+	@Parameters("browser")
+	public void method(String browser)
+	{
+		
+	}
+
+}
---
+++ src/main/java/pom/flipcart/com/PomFlipcart.java
@@ -0,0 +1,15 @@
+package pom.flipcart.com;
+
+import org.testng.annotations.Parameters;
+
+public class PomFlipcart {
+	
+	@Parameters("browsername")
+	public void method1(String browsername)
+	{
+	System.out.println("....");	
+	}
+	
+	
+
+}
